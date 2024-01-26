24cdb48be08a776b9b6c9f8de407e3bc2d6dc97e
Mody <148470115+modymesourceze@users.noreply.github.com>
2024-01-26 13:00:32 +0200

    Add files via upload

---
+++ ELHYBA.py
@@ -0,0 +1,65 @@
+from mody import Mody
+import telebot
+import requests
+import os
+
+tok = Mody.ELHYBA
+
+bot = telebot.TeleBot(tok)
+is_bot_active = True
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, """مرحبًا! أكتب ماتريد البحث عنه سوء كان ملف بايثون او php او أي لغة أخرى وتستطيع البحث عن رات وغيره
+- @elhyba    """)
+
+@bot.message_handler(func=lambda message: True)
+def search_projects(message):
+    global is_bot_active
+    query = message.text
+
+    if query == "/off" and message.from_user.id == 6581896306:
+        is_bot_active = False
+        bot.reply_to(message, "تم إيقاف البوت.")
+    elif query == "/on" and message.from_user.id == 6581896306:
+        is_bot_active = True
+        bot.reply_to(message, "تم تشغيل البوت.")
+    elif is_bot_active:
+        url = f'https://api.github.com/search/repositories?q={query}&sort=stars&order=desc'
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            projects = response.json()['items']
+            message_ms = f'تم العثور على {len(projects)} مشروعًا:\n'
+            
+            for project in projects:
+                name_heroes = project['name']
+                url_pro = project['html_url']
+                message_ms += f'اسم المشروع: {name_heroes}\n'
+                message_ms += f'رابط المشروع: {url_pro}\n'
+                message_ms += '---------------------------------------\n'
+                
+        else:
+            message_ms = 'حدث خطأ أثناء البحث عن المشاريع.'
+            message_ms += 'رمز الحالة:' + str(response.status_code)
+        
+        bot.reply_to(message, message_ms)
+        
+        for project in projects:
+            url_pro = project['html_url']
+            name_heroes = project['name']
+            zip_url = f'{url_pro}/archive/refs/heads/master.zip'
+            r = requests.get(zip_url)
+            
+            with open(f'{name_heroes}.zip', 'wb') as f:
+                f.write(r.content)
+            
+            id_m = message.chat.id
+            bot.send_document(id_m, open(f'{name_heroes}.zip', 'rb'))
+            os.remove(f'{name_heroes}.zip')
+    else:
+        bot.reply_to(message, "البوت متوقف حاليًا.")
+
+
+print("Ch : @Source_Ze")
+bot.polling(True)
